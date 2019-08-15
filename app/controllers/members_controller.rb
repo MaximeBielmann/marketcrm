72679daca43dfc5abb2ed951b8822e7cc1addec4
EC2 Default User <ec2-user@ip-172-31-0-153.us-east-2.compute.internal>
2019-08-15 09:17:31 +0000

    add destroy members from /brands and /peoples

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,4 +3,9 @@ class MembersController < ApplicationController
     Member.create brand_id: params[:brand_id], people_id: params[:people_id], member_role: params[:member_role]
     redirect_to(:back)
   end
+  
+  def delete
+    Member.find(params[:id]).destroy
+    redirect_to(:back)
+  end
 end
